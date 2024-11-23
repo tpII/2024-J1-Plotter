d14832ad9b53f48e405467197629fc398b70ac11
Gian Franco Lasala <gianfrancolasala@hotmail.com>
2024-11-23 09:54:46 -0300

    add lambda dockerfile

--- backend_infra/modules/lambda/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "function_arn" {
-  description = "The ARN of the Lambda function"
-  value       = aws_lambda_function.this.arn
-}
-
-output "function_name" {
-  description = "The name of the Lambda function"
-  value       = aws_lambda_function.this.function_name
-}
