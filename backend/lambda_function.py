089d9dbd92b4ab2a78547ab114a6e7492c00e762
Gian Franco Lasala <gianfrancolasala@hotmail.com>
2024-11-23 17:46:14 -0300

    Commit automático: 2024-11-23 17:46:14

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,5 +1,23 @@
+import boto3
+import json
+import os
+
+# Create IoT Data client
+iot_client = boto3.client('iot-data', region_name='us-east-1')
+
 def lambda_handler(event, context):
+    # Parse incoming request
+    drawing_data = json.loads(event['body'])
+
+    # Publish to MQTT topic
+    response = iot_client.publish(
+        topic='robot/draw',
+        qos=1,
+        payload=json.dumps(drawing_data)
+    )
+
     return {
         'statusCode': 200,
-        'body': 'Hi from lambda!'
+        'body': json.dumps({'message': 'Drawing sent to robot!'})
     }
+}
